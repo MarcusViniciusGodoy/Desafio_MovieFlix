b7f0e7080d455e857b47026174141c91af73d20e
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-06-05 11:47:31 -0300

    FindById Genre

--- MovieFlix/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ MovieFlix/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class GenreService {
@@ -20,5 +23,12 @@ public class GenreService {
     public Page<GenreDTO> findAllPaged(Pageable pageable){
         Page<Genre> list = repository.findAll(pageable);
         return list.map(x -> new GenreDTO());
+    }
+
+    @Transactional(readOnly = true)
+    public GenreDTO findById(Long id) {
+        Optional<Genre> obj = repository.findById(id);
+        Genre entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new GenreDTO();
     }
 }
